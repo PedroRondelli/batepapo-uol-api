fd60e5b7438ccc52a4ff80fe34dfe2d45f50a9e6
Pedro Rondelli <pedrorondelli@hotmail.com>
2022-11-12 19:33:50 -0300

    feat:save the participant in the database

--- index.js
+++ index.js
@@ -1,8 +1,55 @@
 import express from "express";
 import cors from "cors";
+import { MongoClient } from "mongodb";
+import joi from "joi";
+import dotenv from "dotenv";
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
-
 app.listen(5000, () => console.log("It's running maaaaan ..."));
+
+const mongoClient = new MongoClient("mongodb://localhost:27017");
+let db;
+
+try {
+  await mongoClient.connect();
+  db = mongoClient.db("chatUol");
+} catch (erro) {
+  console.log(erro);
+}
+
+const participantSchema = joi.object({
+  name: joi.string().alphanum().min(1).required(),
+});
+
+app.post("/participants", async (req, res) => {
+  const participant = req.body;
+  const validation = participantSchema.validate(participant, {
+    abortEarly: false,
+  });
+
+  if (validation.error) {
+    const erros = validation.error.details.map((detail) => detail.message);
+    res.status(422).send(erros);
+    return;
+  }
+
+  try {
+    const participantExist = await db.collection("participants").findOne({
+      name: participant.name,
+    });
+
+    if (participantExist) { 
+      res.sendStatus(409);
+      return;
+    }
+    await db.collection("participants").insertOne({
+      ...participant,
+      lastStatus: Date.now(),
+    });
+    res.sendStatus(201);
+  } catch (error) {
+    console.log(error);
+  }
+});

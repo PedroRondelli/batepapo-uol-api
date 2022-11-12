a16000b8f92a28645dabf5b14042394141f8f824
Pedro Rondelli <pedrorondelli@hotmail.com>
2022-11-12 20:05:35 -0300

    feat: returns the list of all participants

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ import cors from "cors";
 import { MongoClient } from "mongodb";
 import joi from "joi";
 import dotenv from "dotenv";
+import dayjs from "dayjs";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+dotenv.config();
 app.listen(5000, () => console.log("It's running maaaaan ..."));
 
-const mongoClient = new MongoClient("mongodb://localhost:27017");
+const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
 
 try {
@@ -40,7 +42,7 @@ app.post("/participants", async (req, res) => {
       name: participant.name,
     });
 
-    if (participantExist) { 
+    if (participantExist) {
       res.sendStatus(409);
       return;
     }
@@ -48,8 +50,28 @@ app.post("/participants", async (req, res) => {
       ...participant,
       lastStatus: Date.now(),
     });
+    await db.collection("messages").insertOne({
+      from: participant.name,
+      to: "Todos",
+      text: "entra na sala...",
+      type: "status",
+      time: dayjs().format("HH:mm:ss"),
+    });
     res.sendStatus(201);
   } catch (error) {
     console.log(error);
+  }
+});
+
+app.get("/participants", async (req, res) => {
+  try {
+    const allParticipants = await db
+      .collection("participants")
+      .find()
+      .toArray();
+    console.log(allParticipants)  
+    res.send(allParticipants);
+  } catch (erro) {
+    console.log(erro);
   }
 });
